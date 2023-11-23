9a5f08cdc15fbab756942f472e32a170599192f7
FabianK-git <91976440+FabianK-git@users.noreply.github.com>
2023-11-23 15:34:35 +0100

    Changes to cli argument names.

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,18 @@ use terminal_link::Link;
 use ctrlc;
 use normpath::PathExt;
 
+struct SearchTerm {
+    regex: Option<Regex>,
+    search_string: Option<String>
+}
+
 fn main() -> Result<()> {
     // Setup Ctrl + C interrupt handling
     ctrlc::set_handler(move || {
         execute!(
             stdout(),
             SetForegroundColor(Color::Red),
-            Print("\r\nKeyboard Interrupt"),
+            Print("\r\nKeyboard Interrupt\n"),
             ResetColor
         ).unwrap();
 
@@ -35,32 +40,30 @@ fn main() -> Result<()> {
     let args = parse_arguments(env::args());
 
     // Start search if pattern or name is specified
-    if args.contains_key("name") || args.contains_key("pattern") {
+    if args.contains_key("name") || args.contains_key("regex") {
         // Set default values
         let mut directory = env::current_dir()?;
-        let mut pattern: Option<Regex> = None;
+        let mut search: SearchTerm = SearchTerm { regex: None, search_string: None };
 
-        if args.contains_key("dir") {
-            directory = Path::new(args.get("dir").unwrap()).to_path_buf();
+        if args.contains_key("path") {
+            directory = Path::new(args.get("path").unwrap()).to_path_buf();
         }
 
         if args.contains_key("name") {
-            pattern = Some(Regex::new(args.get("name").unwrap()).unwrap());
+            search.search_string = Some(String::from(args.get("name").unwrap()));
         }
 
-        if args.contains_key("pattern") {
-            pattern = Some(Regex::new(args.get("pattern").unwrap()).unwrap());
+        if args.contains_key("regex") {
+            search.regex = Some(Regex::new(args.get("regex").unwrap()).unwrap());
         }
 
-        if let Some(pattern) = pattern {
-            traverse_filesystem(directory, &pattern);
-        }
+        traverse_filesystem(directory, &search);
 
         execute!(
             stdout(),
             Clear(ClearType::CurrentLine),
             SetForegroundColor(Color::DarkGreen),
-            Print("\rProcess finished")
+            Print("\rProcess finished\n")
         )?;
     }
     else {
@@ -68,7 +71,7 @@ fn main() -> Result<()> {
             stdout(),
             Clear(ClearType::CurrentLine),
             SetForegroundColor(Color::Yellow),
-            Print("No name or pattern was provided.\nUse --help to get usage."),
+            Print("No name or pattern was provided.\nUse --help to get usage.\n"),
             ResetColor
         )?;
     }
@@ -81,28 +84,28 @@ fn parse_arguments(args: env::Args) -> HashMap<String, String> {
 
     let mut arguments: HashMap<String, String> = HashMap::new();
 
-    if args.len() <= 2 || args[1] == "--help" {
+    if args.len() <= 2 || args[1] == "--help" || args[1] == "-h" {
         execute!(
             stdout(),
             Print("Usage of find-rs:\n"),
 
-            SetForegroundColor(Color::Grey),
-            Print("--help\t\t\t"),
+            SetForegroundColor(Color::DarkGreen),
+            Print("--help, -h\t\t\t"),
             ResetColor,
             Print("Shows this output.\n"),
 
-            SetForegroundColor(Color::Grey),
-            Print("--dir PATH\t\t"),
+            SetForegroundColor(Color::DarkGreen),
+            Print("--path, -p [PATH]\t\t"),
             ResetColor,
             Print("Specifies the directory where the programs starts. (Optional)\n"),
 
-            SetForegroundColor(Color::Grey),
-            Print("--name FILENAME\t\t"),
+            SetForegroundColor(Color::DarkGreen),
+            Print("--name, -n [FILENAME]\t\t"),
             ResetColor,
             Print("Specifies the name or a part of the name which should be searched.\n"),
 
-            SetForegroundColor(Color::Grey),
-            Print("--pattern REGEX-PATTERN\t"),
+            SetForegroundColor(Color::DarkGreen),
+            Print("--regex, -r [PATTERN]\t\t"),
             ResetColor,
             Print("Specifies a regex pattern that will be used for the search.\n"),
         ).unwrap();
@@ -112,10 +115,23 @@ fn parse_arguments(args: env::Args) -> HashMap<String, String> {
     }
 
     for i in 0..args.len() {
-        if args[i].starts_with("--") && i + 1 < args.len() {
+        if (args[i].starts_with("--") ) && i + 1 < args.len() {
             args[i].replace_range(0..2, "");
             
             let key: String = args[i].clone();
+
+            arguments.insert(key, args[i + 1].clone());
+        }
+        else if args[i].starts_with("-") && i + 1 < args.len() {
+            args[i].replace_range(0..1, "");
+            
+            let key: String = String::from(match args[i].clone().as_str() {
+                "h" => "help",
+                "p" => "path",
+                "n" => "name",
+                "r" => "regex",
+                _ => ""
+            });
 
             arguments.insert(key, args[i + 1].clone());
         }
@@ -124,16 +140,21 @@ fn parse_arguments(args: env::Args) -> HashMap<String, String> {
     return arguments;
 }
 
-fn traverse_filesystem(current_dir: PathBuf, pattern: &Regex) {
+fn traverse_filesystem(current_dir: PathBuf, search: &SearchTerm) {
     let entries = fs::read_dir(current_dir);
-
+    
     if let Ok(entries) = entries {
         for entry in entries {
             match entry {
                 Ok(entry) => {
-                    if pattern.is_match(entry.file_name().to_str().unwrap()) {
-                        let file_path = entry.path();
+                    if search.regex.as_ref().is_some_and(|pattern| pattern.is_match(entry.file_name().to_str().unwrap())) || 
+                       search.search_string.as_ref().is_some_and(|string| string.find(entry.file_name().to_str().unwrap()).is_some()) 
+                    {
+                        // if search.search_string.is_some() {
+                        //     println!("{:?}", search.search_string);
+                        // }
 
+                        let file_path = entry.path();
                         let absolute_path = file_path.normalize();
 
                         match absolute_path {
@@ -163,7 +184,7 @@ fn traverse_filesystem(current_dir: PathBuf, pattern: &Regex) {
                     // Call this function again if a folder is found
                     if let Ok(entry_type) = entry.file_type() {
                         if entry_type.is_dir() {
-                            traverse_filesystem(entry.path(), pattern);
+                            traverse_filesystem(entry.path(), search);
                         }
                     }
                 },
